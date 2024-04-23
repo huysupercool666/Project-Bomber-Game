1ffed8d51e4fdee0c1bb48cad885a61f0e8bf2c3
Huysupercool666 <huyTD30112001@gmail.com>
2024-04-24 00:55:50 +0700

    Create Map

--- playGames.js
+++ playGames.js
@@ -1,5 +1,12 @@
-import tileMap from "./tileMap.js";
+import TileMap from "./TileMap.js";
 
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
-const tileMap = new tileMap();
+const tileSize = 32;
+const tileMap = new TileMap(tileSize);
+
+function gameLoop() {
+    tileMap.draw(canvas, ctx);
+}
+
+setInterval(gameLoop, 1000 / 60);
--- tileMap.js
+++ tileMap.js
@@ -1,1 +1,64 @@
-export default class tileMap {}
+export default class TileMap {
+  constructor(tileSize) {
+    this.tileSize = tileSize;
+    this.hardWall = this.#image("HardWall.png");
+  }
+
+  #image(fileName) {
+    const img = new Image();
+    img.src = `Image/${fileName}`;
+    return img;
+  }
+
+  //use 2 mension array to draw a map
+  // 0 is hard wall
+  // 1 is soft wall
+  map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ];
+
+  draw(canvas, ctx) {
+    this.#setCanvasSize(canvas);
+    this.#clearCanvas(canvas, ctx);
+    this.#drawMap(ctx);
+  }
+  #drawMap(ctx) {
+    for (let row = 0; row < this.map.length; row++) {
+      for (let column = 0; column < this.map[row].length; column++) {
+        const tile = this.map[row][column];
+        let image = null;
+        switch (tile) {
+          case 1:
+            image = this.hardWall;
+            break;
+        }
+        if (image != null)
+          ctx.drawImage(
+            image,
+            column * this.tileSize,
+            row * this.tileSize,
+            this.tileSize,
+            this.tileSize
+          );
+      }
+    }
+  }
+
+  #clearCanvas(canvas, ctx) {
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+
+  #setCanvasSize(canvas) {
+    canvas.height = this.map.length * this.tileSize;
+    canvas.width = this.map[0].length * this.tileSize;
+  }
+}
